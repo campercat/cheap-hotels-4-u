1c5f5877cf3cb8b60c7d8da5325c2b7f352c1e35
codesquadkiki <47373509+codesquadkiki@users.noreply.github.com>
2021-03-31 15:17:11 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Cheap Hotesl 4 U
+Cheap Hotels 4 U specializes in listing cheap hotels in beautiful destinations around the globe. Before building their new website, they want to hire an outside tech firm to create a model of the hotels to keep their data organized on the website. Use your newly-minted JavaScript object skills to help your firm submit an RFP (i.e. a proposal).
+
+## Directions
+In this exercise, you will create a data model for a series of hotels using JavaScript objects.  For each hotel, you will need to track, for example, how many rooms it has, how many rooms are booked, and whether it has a pool. This is a tangible example of how you might use objects when you're working as a professional coder.
+
+## Submission
+Push your code to your Github account and then paste the link to the repo when submitting. If you do not finish in class, submit what you were able to get done.
+
+### Assignment Hints
+- You will need to use a loop for several parts of this assignment
+- Some knowledge of arrays will be needed like accessing an array
+- console.log your results 
---
+++ scripts.js
@@ -0,0 +1,67 @@
+// START HERE
+// STEP 1. Create a variable called 'hotels' that is an array of objects. Enter in the following information for the 3 hotels:
+
+// HOTEL #1 - Name is Hotel Paradise, there are 50 total rooms and there are 0 booked rooms
+// HOTEL #2 - Name is Quay Hotel, there are 75 total rooms and there are 0 booked rooms
+// HOTEL #3 - Name is Parker Place, there are 140 total rooms and there are 0 booked rooms
+
+
+// STEP 2. We missed some additional information for each hotel but its okay! Add the following information below for each hotel object. Do not manipulate the code in Step 1.
+
+// HOTEL PARADISE
+// -has a gym - NO
+// -has a pool - YES
+// -allows pets - NO
+
+
+// QUAY HOTEL
+// -has a gym - YES
+// -has a pool - YES
+// -allows pets - YES
+
+
+// PARKER PLACE
+// -has a gym - YES
+// -has a pool - YES
+// -allows pets - YES
+
+
+// STEP 3. Print the name of each hotel on a separate line
+console.log("-------------\n");
+console.log("3. Hotel Names");
+
+
+// STEP 4. Print the total number of rooms each hotel has
+console.log("-------------\n");
+console.log("4. Total Rooms");
+
+
+// STEP 5. Print the names of hotels that have gyms
+console.log("-------------\n");
+console.log("5. Hotels with Gyms");
+
+
+// STEP 6. Since building this new website, customers are starting to book rooms. Cheap Hotels 4 U has provided the number of rooms booked for each hotel. Update each object with the numbers of booked rooms.
+
+// HOTEL PARADISE = 6 booked rooms
+
+
+// QUAY HOTEL = 15 booked rooms
+
+
+// PARKER PLACE = 35 booked rooms
+
+
+// STEP 7. Now print the number of rooms that are booked for each hotel to confirm that the system has been updated.
+console.log("-------------\n");
+console.log("7. Rooms Booked");
+
+
+// STEP 8. Calculate and print the number of rooms available per hotel
+console.log("-------------\n");
+console.log("8. Rooms Available");
+
+
+// STEP 9. The Marketing Team believes the new site has 500 rooms, but the Construction Team says less than 300. To figure out the actual total, print the total number of rooms by adding this across all 3 hotels.
+console.log("-------------\n");
+console.log("9. Total Rooms Across All Hotels");
